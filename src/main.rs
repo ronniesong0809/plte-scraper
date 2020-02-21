d5d594d140a30a7cb1185a2d791d9584304cbf2c
Ronnie Song <ronniesong0809@gmail.com>
2020-02-21 02:16:51 -0800

    refactor: fix matching pattern, ok() result return
    move csv and html to assert/

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,16 @@ extern crate select;
 use bson::{bson, doc};
 use mongodb::{options::ClientOptions, Client};
 // use mongodb::options::FindOptions;
+use csv::Writer;
+use select::document::Document;
+#[allow(unused_imports)]
+use select::predicate::{Attr, Class, Name, Predicate};
 use serde::Deserialize;
 use std::error::Error;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::{stdin, stdout, Write};
 use std::process;
-use select::document::Document;
-#[allow(unused_imports)]
-use select::predicate::{Predicate, Attr, Class, Name};
-use csv::Writer;
 
 #[derive(Debug, Deserialize)]
 #[allow(non_snake_case)]
@@ -29,17 +29,32 @@ struct Record {
 }
 
 #[allow(bare_trait_objects)]
-pub fn to_csv() -> Result<(), Box<Error>>  {
-    let document = Document::from(include_str!("Calendar.html"));
-    let mut wtr = Writer::from_path("foo.csv")?;
-    wtr.write_record(&["summary", "location", "days_of_week", "month", "day", "year", "start", "end"])?;
+pub fn parse_date() -> Result<String, Box<Error>> {
+    let document = Document::from(include_str!("../assets/Calendar.html"));
+    let mut wtr = Writer::from_path("assets/Calendar.csv")?;
+    wtr.write_record(&[
+        "summary",
+        "location",
+        "days_of_week",
+        "month",
+        "day",
+        "year",
+        "start",
+        "end",
+    ])?;
 
-    println!("# Top 10 Event");
+    let mut count = 0;
     for node in document.find(Class("vevent")) {
         let summary = node.find(Class("summary")).next().unwrap().text();
         let location = node.find(Class("location")).next().unwrap().text();
         let days_of_week = node.find(Class("day_of_week")).next().unwrap().text();
-        let temp_date = node.find(Class("list_date")).next().unwrap().last_child().unwrap().text();
+        let temp_date = node
+            .find(Class("list_date"))
+            .next()
+            .unwrap()
+            .last_child()
+            .unwrap()
+            .text();
         let mut date = temp_date.split_ascii_whitespace();
         let month = date.next().unwrap();
         let day = date.next().unwrap();
@@ -47,16 +62,39 @@ pub fn to_csv() -> Result<(), Box<Error>>  {
         let start = node.find(Class("dtstart")).next().unwrap().text();
         let end = node.find(Class("dtend")).next().unwrap().text();
 
-        println!("     summary: {}", summary);
+        println!("\n     summary: {}", summary);
         println!("    location: {}", location);
         println!("days_of_week: {}", days_of_week);
         println!("        date: {} {} {}", month, day, year);
         println!("        time: {} - {}", start, end);
-        println!("");
-        wtr.write_record(&[summary, location, days_of_week, month.to_string(), day.to_string(), year.to_string(), start, end])?;
+        wtr.write_record(&[
+            summary,
+            location,
+            days_of_week,
+            month.to_string(),
+            day.to_string(),
+            year.to_string(),
+            start,
+            end,
+        ])?;
+        count += 1;
     }
     wtr.flush()?;
-    Ok(())
+    Ok(format!(
+        "\nSuccessfully saved {} events to assets/Calendar.csv",
+        count
+    ))
+}
+
+fn save_to_csv() {
+    let status = parse_date();
+    match status {
+        Ok(v) => println!("{}", v),
+        Err(e) => {
+            println!("error running example: {}", e);
+            process::exit(1);
+        }
+    }
 }
 
 fn mongodb_driver() -> mongodb::Client {
@@ -75,21 +113,26 @@ fn coll() -> mongodb::Collection {
 }
 
 fn open_file() {
-    if let Err(err) = insert(File::open("csv_test.csv").expect("Failed to read csv.")) {
-        println!("error running example: {}", err);
-        process::exit(1);
+    let status = insert(File::open("assets/Calendar.csv").expect("Failed to read csv."));
+    match status {
+        Ok(v) => println!("{}", v),
+        Err(e) => {
+            println!("error running example: {}", e);
+            process::exit(1);
+        }
     }
 }
 
-fn insert(file: File) -> Result<(), Box<dyn Error>> {
+fn insert(file: File) -> Result<&'static str, Box<dyn Error>> {
     let coll = coll();
-    coll.delete_many(doc! {}, None).expect("Failed to delete documents.");
+    coll.delete_many(doc! {}, None)
+        .expect("Failed to delete documents.");
     let mut rdr = read_file(file);
     for result in rdr.deserialize() {
         let record: Record = result?;
         coll.insert_one(doc! { "summary": record.summary, "location": record.location, "days_of_week": record.days_of_week, "month": record.month, "day": record.day, "year": record.year, "start": record.start, "end": record.end }, None).unwrap();
     }
-    Ok(())
+    Ok("\nSuccessfully saved to MongoDB")
 }
 
 fn read_file(file: File) -> csv::Reader<std::io::BufReader<std::fs::File>> {
@@ -104,7 +147,11 @@ fn display() {
     }
 }
 
-fn search(find: &str) {
+fn search() {
+    print!("\nPlease enter: ");
+    let find_input = user_input();
+    let find = find_input.trim();
+    // print!("{}", find);
     let coll = coll();
     let filter = doc! { "summary": find };
     // let find_options = FindOptions::builder()
@@ -120,7 +167,7 @@ fn search(find: &str) {
     }
 }
 
-fn user_input () -> std::string::String{
+fn user_input() -> std::string::String {
     stdout().flush().unwrap();
     let mut input = String::new();
     stdin().read_line(&mut input).expect("Failed to read line");
@@ -142,16 +189,10 @@ fn main() {
         let command = input.trim().split_whitespace().next().unwrap();
 
         match &*command {
-            "1" => to_csv().unwrap(),
+            "1" => save_to_csv(),
             "2" => open_file(),
             "3" => display(),
-            "4" => {
-                print!("\nPlease enter: ");
-                let find_input = user_input();
-                let find = find_input.trim();
-                // print!("{}", find);
-                search(find);
-            }
+            "4" => search(),
             "0" => return,
             "q" => return,
             "quit" => return,
